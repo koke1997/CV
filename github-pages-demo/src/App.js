62391028b5e426bc822953b6f1846bced99a1e4e
koke1997 <67540157+koke1997@users.noreply.github.com>
2023-10-17 20:12:44 +0200

    saddsa

---
+++ github-pages-demo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, GitHub Pages!</h1>
+        <p>This is a simple React app deployed on GitHub Pages.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
