1f87cc2163f1b0d3b09db6ae5676a486fc3d7231
koke1997 <67540157+koke1997@users.noreply.github.com>
2023-11-04 16:18:39 +0100

    Update firebaseConfig.js

--- github-pages-react-app/src/firebaseConfig.js
+++ github-pages-react-app/src/firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAPTmwqiqC5lTKD4j4LqY7fWj15zMXiTfs",
-    authDomain: "quickstart-1574936872651.firebaseapp.com",
-    databaseURL: "https://quickstart-1574936872651-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "quickstart-1574936872651",
-    storageBucket: "quickstart-1574936872651.appspot.com",
-    messagingSenderId: "114721513330",
-    appId: "1:114721513330:web:6c89ea1cba46e2319dc7eb",
-    measurementId: "G-LN24KTP8EW"
+    apiKey: "api key here",
+    authDomain: "quickstart-xx.firebaseapp.com",
+    databaseURL: "https://quickstart-xx-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "quickstart-xx",
+    storageBucket: "quickstart-xx.appspot.com",
+    messagingSenderId: "xx",
+    appId: "1:xx:web:xx",
+    measurementId: "G-xx"
 };
 
 export default firebaseConfig;
