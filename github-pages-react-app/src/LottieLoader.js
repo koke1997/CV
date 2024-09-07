8181dfb959b988c329bc81d497db14c984f706cb
koke1997 <67540157+koke1997@users.noreply.github.com>
2024-09-07 20:46:45 +0200

    Delete github-pages-react-app directory

--- github-pages-react-app/src/LottieLoader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Lottie from 'lottie-react';
-import animationData from './Lottie_Lego.json';
-
-function LottieLoader() {
-    return <Lottie animationData={animationData} />;
-}
-
-export default LottieLoader;
